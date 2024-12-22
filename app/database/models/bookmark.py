5b423f3a7298ff34ba67e15259a045c9138fc436
kondratyevan <kondratyevan@ozon.ru>
2024-12-22 20:22:27 +0300

    Отдельное имя схемы

--- app/database/models/bookmark.py
+++ app/database/models/bookmark.py
@@ -5,20 +5,25 @@ from app.database.models import Base
 
 class Shelf(Base):
     __tablename__ = 'shelf'
+    __table_args__ = ({"schema": "personal_account"})
+
     id = Column(Integer, primary_key=True, autoincrement=True)
-    fk_user = Column(Integer, ForeignKey('users.id'), nullable=False)
+    fk_user = Column(Integer, ForeignKey('personal_account.users.id'), nullable=False)
     name = Column(String)
 
 
 class Bookmark(Base):
     __tablename__ = 'bookmarks'
+    __table_args__ = ({"schema": "personal_account"})
+
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
 
 
 class BookmarkInShelf(Base):
     __tablename__ = 'bookmarks_inshelf'
-    fk_shelf = Column(Integer, ForeignKey('shelf.id', onupdate='CASCADE', ondelete='CASCADE'),
+    __table_args__ = ({"schema": "personal_account"})
+    fk_shelf = Column(Integer, ForeignKey('personal_account.shelf.id', onupdate='CASCADE', ondelete='CASCADE'),
                       primary_key=True, nullable=False)
-    fk_bookmark = Column(Integer, ForeignKey('bookmarks.id', onupdate='CASCADE', ondelete='CASCADE'),
+    fk_bookmark = Column(Integer, ForeignKey('personal_account.bookmarks.id', onupdate='CASCADE', ondelete='CASCADE'),
                          primary_key=True, nullable=False)
